b21cb625a15d7006ed2dd273218b6c1f4ed51d00
alfredosdpiii <alfredo.sdpiii@gmail.com>
2024-10-08 16:10:00 +0800

    add profile route

--- src/components/componentGroups/AuthButtons.tsx
+++ src/components/componentGroups/AuthButtons.tsx
@@ -3,6 +3,7 @@ import { useUser } from "@auth0/nextjs-auth0/client";
 import { SignupButton } from "@/components/buttons/signup-button";
 import { LoginButton } from "@/components/buttons/login-button";
 import { LogoutButton } from "@/components/buttons/logout-button";
+import Link from "next/link";
 
 export const AuthButtons = () => {
   const { user } = useUser();
@@ -16,6 +17,7 @@ export const AuthButtons = () => {
       )}
       {user && (
         <>
+          <Link href="/profile">Profile</Link>
           <LogoutButton />
         </>
       )}
